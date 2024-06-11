b8d6650d8c0d2a1f9f30491ea9589b8557d52a30
Lee_Tsung_Yu <jt25346798@gmail.com>
2024-06-11 15:45:45 +0800

    2024/06/11 15:45 updated

--- NCKU/碩一下/Categorical data analysis/homework solution/ch5.R
+++ NCKU/碩一下/Categorical data analysis/homework solution/ch5.R
@@ -19,3 +19,39 @@ Crabs2$Year <- Crabs2$Year - 1993
 fit <- glm(y ~ ., family = binomial, data = Crabs2)
 stepAIC(fit)
 
+# 5.7
+
+# 5.9
+
+# 5.15
+data <- data.frame(Center = rep(c(1:5),each = 2),
+                   treatment = rep(c("Drug","Placebo"),5),
+                   case = c(5,9,13,10,7,5,9,8,14,14),
+                   y = c(0,0,1,0,0,0,6,2,5,2))
+data$treatment <- relevel(as.factor(data$treatment), ref = "Placebo")
+
+fit <- glm(y/case ~ factor(Center) + factor(treatment), weights = case, family = binomial, data = data)
+summary(fit)
+
+# b.
+fit <- glm(y/case ~ factor(Center) + factor(treatment)-1, weights = case, family = binomial, data = data)
+summary(fit)
+
+# c.
+data <- data.frame(Center = rep(c(2,4,5),each = 2),
+                   treatment = rep(c("Drug","Placebo"),3),
+                   case = c(13,10,9,8,14,14),
+                   y = c(1,0,6,2,5,2))
+data$treatment <- relevel(as.factor(data$treatment), ref = "Placebo")
+fit <- glm(y/case ~ factor(Center) + factor(treatment)-1, weights = case, family = binomial, data = data)
+summary(fit)
+
+# 5.17
+sorethroat <- read.table("C:/github_LTY/Lee_Tsung_Yu/NCKU/碩一下/Categorical data analysis/dataset/SoreThroat.dat", header = T)
+fit1 <- lm(Y ~ D+T, data = sorethroat)
+summary(fit1)
+fit2 <- glm(Y ~ D+T, family = binomial, data = sorethroat)
+summary(fit2)
+
+# 5.19
+
